8ab8a54cb822e5e22f5bf4f513c54c72281b177e
FAFernandez247 <felicityfernandez247@gmail.com>
2025-10-23 12:05:43 +0800

    assessment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import './App.css'
 
 function App() {
-  return <>{/* Add your page components here */}</>
+  return (
+    <div className="container">
+      <div className="card">
+        <img src="images/fruit.png" className="fruit-image"></img>
+        <p className='title'>Gum-Gum Fruit</p>
+        <p className='title-translate'>(Gomu Gomu no Mi)</p>
+        <p className='description'>
+          "Gomu Gomu no Mi, also known as the Fruit of Gum-Gum in French, is a Paramecia-type Demon Fruit that imparts the same properties as rubber to its user's body, making the eater an Elastic Man (ゴム人間, Gomu Ningen). This was once a treasure that Shanks and his crew had taken from a World Government convoy, protected by CP9, but which was accidentally eaten by Monkey D. Luffy"
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App
